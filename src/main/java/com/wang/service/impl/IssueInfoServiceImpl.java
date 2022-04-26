4f6b2b6b7bfe37ca31411c67e4eb43cc6e799f8a
HappyTsing <593768341@qq.com>
2022-04-26 15:04:15 +0800

    简单尝试日志

--- src/main/java/com/wang/service/impl/IssueInfoServiceImpl.java
+++ src/main/java/com/wang/service/impl/IssueInfoServiceImpl.java
@@ -4,16 +4,19 @@ import com.wang.dao.ClassifyMapper;
 import com.wang.dao.IssueInfoMapper;
 import com.wang.pojo.IssueInfo;
 import com.wang.service.IssueInfoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class IssueInfoServiceImpl implements IssueInfoService {
+    private static  final Logger logger = LoggerFactory.getLogger(IssueInfoServiceImpl.class);
     @Autowired
     private IssueInfoMapper issueInfoMapper;
     @Override
     public IssueInfo  getdata(){
-        System.out.println("进入getdata");
+        logger.info("进入getdata");
         IssueInfo issueInfo = issueInfoMapper.selectByPrimaryKey(1);
         System.out.println(issueInfo);
         return issueInfo;
